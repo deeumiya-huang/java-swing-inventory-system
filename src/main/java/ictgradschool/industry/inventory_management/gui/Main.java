b04300100acf19637de05e614ee4621e45fa09ca
Deeumiya Huang <Abyukuri@gmail.com>
2026-06-06 19:03:31 +1200

    feat: create JFileChooser for selecting a new filestore

--- src/main/java/ictgradschool/industry/inventory_management/gui/Main.java
+++ src/main/java/ictgradschool/industry/inventory_management/gui/Main.java
@@ -8,6 +8,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
 import java.util.*;
 import java.util.List;
 
@@ -99,6 +100,49 @@ public class Main extends JFrame {
                         setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR)); // change cursor to waiting mode to let user know that the file is loading.
                         products = FilestoreManager.readData(file); //todo: do i have to use worker doInBackground?
                         setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+                        changeToSystemSelectPanel();
+                    }
+                }
+            });
+
+            newFileBtn.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JFileChooser fileChooser = new JFileChooser();
+                    int returnVal = fileChooser.showSaveDialog(Main.this);
+                    if (returnVal == JFileChooser.APPROVE_OPTION) {
+                        File selectedFile = fileChooser.getSelectedFile();
+                        // if user forget to type ".json", add it on automatically
+                        if (!selectedFile.getName().endsWith(".json")) {
+                            selectedFile = new File(selectedFile.getAbsolutePath() + ".json");
+                        }
+                        setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+
+                        if (selectedFile.exists()) {
+                            int response = JOptionPane.showConfirmDialog(Main.this,
+                                    "The file already exists! Would you like to overwrite it and create a new filestore? (Note: Old data will be lost)",
+                                    "Confirm Overwrite",
+                                    JOptionPane.YES_NO_OPTION,
+                                    JOptionPane.WARNING_MESSAGE
+                                    );
+                            if (response == JOptionPane.NO_OPTION) {
+                                setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+                                return;
+                            }
+                        } else {
+                            try {
+                                boolean isCreated = selectedFile.createNewFile();
+                                if (!isCreated) {
+                                    throw  new IOException("Failed to create a new file.");
+                                }
+                            } catch (IOException exception) {
+                                exception.printStackTrace();
+                            }
+                        }
+                        setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+
+                        file = selectedFile;
+                        products = new ArrayList<>();
                         changeToSystemSelectPanel();
                     }
                 }
