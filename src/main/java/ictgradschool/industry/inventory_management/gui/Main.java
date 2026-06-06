33a19b267600acd4e20c8ddfa880f8d3cce37bf6
Deeumiya Huang <Abyukuri@gmail.com>
2026-06-06 14:13:38 +1200

    feat: use CardLayout to change screen panel between filestoreSelect and systemSelect

--- src/main/java/ictgradschool/industry/inventory_management/gui/Main.java
+++ src/main/java/ictgradschool/industry/inventory_management/gui/Main.java
@@ -12,50 +12,111 @@ import java.util.*;
 import java.util.List;
 
 public class Main extends JFrame {
-    List<Product> products;
-    JButton newFileBtn;
-    JButton existingFileBtn;
-    public Main() {
-        JPanel panel = new JPanel();
-        newFileBtn = new JButton("Create a new Filestore");
-        existingFileBtn = new JButton("Select an existing filestore");
-        buildGui(panel, newFileBtn, existingFileBtn);
+    private File file;
+    private List<Product> products;
 
-        existingFileBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser fileChooser = new JFileChooser();
-                int returnVal = fileChooser.showOpenDialog(Main.this);
-                if (returnVal == JFileChooser.APPROVE_OPTION) {
-                    File file = fileChooser.getSelectedFile(); // todo: do I have to try catch here? But I already try catch in FilestoreManager
-                    setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR)); // change cursor to waiting mode to let user know that the file is loading.
-                    products = FilestoreManager.readData(file); //todo: do i have to use worker doInBackground?
-                    setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-//                    changeToSelectScreen();
-                }
-            }
-        });
+    private final JPanel mainContainer;
+    private final CardLayout cardLayout;
+    private final JPanel filestoreSelect;
+    private final JPanel systemSelect;
+
+    public Main() {
+        cardLayout = new CardLayout();
+        mainContainer = new JPanel(cardLayout);
+
+        filestoreSelect = new FilestoreSelect();
+        systemSelect = new SystemSelect();
+
+        buildGui(mainContainer, filestoreSelect, systemSelect);
     }
 
-    private void buildGui(JPanel panel, JButton btn1, JButton btn2) {
-        panel.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.insets = new Insets(10, 10, 10, 10);
-        panel.add(btn1, gbc);
-
-        gbc.gridy = 1;
-        panel.add(btn2, gbc);
+    private void buildGui(JPanel mainContainer, JPanel filestoreSelect, JPanel systemSelect) {
+        mainContainer.add(filestoreSelect, "FILESTORE_PANEL");
+        mainContainer.add(systemSelect, "SYSTEM_PANEL");
 
         setTitle("Welcome to inventory system");
         setPreferredSize(new Dimension(400,300));
-        setContentPane(panel);
+        setContentPane(mainContainer);
         pack();
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
+    }
+
+    private void changeToSystemSelectPanel() {
+        cardLayout.show(mainContainer, "SYSTEM_PANEL");
+    }
+
+    private void changeToFilestoreSelectPanel() {
+        cardLayout.show(mainContainer, "FILESTORE_PANEL");
+    }
+
+    public class SystemSelect extends JPanel {
+        JButton backBtn;
+        JButton inventoryManagerBtn;
+        JButton posBtn;
+
+        public SystemSelect() {
+            backBtn = new JButton("Close filestore");
+            inventoryManagerBtn = new JButton("Open Inventory Manager");
+            posBtn = new JButton("Open Point of Sale");
+            buildPanelGui(backBtn, inventoryManagerBtn, posBtn);
+        }
+
+        private void buildPanelGui(JButton backBtn, JButton inventoryManagerBtn, JButton posBtn) {
+            setLayout(new GridBagLayout());
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            gbc.insets = new Insets(10, 10, 10, 10);
+            add(backBtn, gbc);
+
+            gbc.gridy = 1;
+            add(inventoryManagerBtn, gbc);
+
+            gbc.gridy = 2;
+            add(posBtn, gbc);
+        }
+    }
+
+    public class FilestoreSelect extends JPanel {
+        JButton newFileBtn;
+        JButton existingFileBtn;
+
+        public FilestoreSelect() {
+            newFileBtn = new JButton("Create a new Filestore");
+            existingFileBtn = new JButton("Select an existing filestore");
+            buildPanelGui(newFileBtn, existingFileBtn);
+
+            existingFileBtn.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JFileChooser fileChooser = new JFileChooser();
+                    int returnVal = fileChooser.showOpenDialog(Main.this);
+                    if (returnVal == JFileChooser.APPROVE_OPTION) {
+                        file = fileChooser.getSelectedFile(); // todo: do I have to try catch here? But I already try catch in FilestoreManager
+                        setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR)); // change cursor to waiting mode to let user know that the file is loading.
+                        products = FilestoreManager.readData(file); //todo: do i have to use worker doInBackground?
+                        setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+                        changeToSystemSelectPanel();
+                    }
+                }
+            });
+        }
+
+        private void buildPanelGui(JButton newFileBtn, JButton existingFileBtn) {
+            setLayout(new GridBagLayout());
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            gbc.insets = new Insets(10, 10, 10, 10);
+            add(newFileBtn, gbc);
+
+            gbc.gridy = 1;
+            add(existingFileBtn, gbc);
+        }
     }
 
     public static void main(String[] args) {
